bce8f400738fa6e4dbec1ba3f7f1e8ff9d119dbb
unknown <fidelamendola@yahoo.com>
2020-10-23 16:15:53 -0300

    proyect finished

---
+++ script.js
@@ -0,0 +1,84 @@
+
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+
+// Unsplash API 
+
+let count = 5;
+const apiKey = 'N09v6PUbGFGgYKQyVlMDWabi3pZtYET0p5cHsbzvqCQ';
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all the img were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    initialLoad = false;
+    count = 30;
+    }
+};
+
+
+// Helper function to set attributes on DOM elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    };
+};
+
+
+// Create Elements for Links & Photos, add to DOM
+
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    photosArray.forEach((photo) => {
+    const item = document.createElement('a');
+    setAttributes(item, {
+    href: photo.links.html,
+    target: '_blank'
+    });
+    
+    const img = document.createElement('img');
+    setAttributes(img, {
+    src: photo.urls.regular,
+    alt: photo.alt_description,
+    title: photo.alt_description
+    });
+    
+    img.addEventListener('load', imageLoaded);
+
+
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+    });
+};
+
+
+async function getPhotos() {
+    try {
+      const response = await fetch(apiUrl);
+      photosArray = await response.json();
+      displayPhotos();
+    } catch (error) {
+    // Catch error here
+    }
+};
+
+
+window.addEventListener('scroll', () => {
+   if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    getPhotos();
+    ready = false;
+        } 
+      });
+
+
+getPhotos();
